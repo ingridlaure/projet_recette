bdac8ed12e673e71ff63caf438901dbbe555ce1c
ingrid <ingrid.ngounekenfack@condorce.be>
2024-04-23 10:48:37 +0200

    accueil

---
+++ pages/accueil.php
@@ -0,0 +1,38 @@
+
+bienvenu sur le site des recette
+<form>
+    <h3>Filtrer par : </h3>
+    <form>
+        <label for="categorie">categorie</label>
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <label for="niveau"> niveau</label>
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <label for="Nombre de part"> nombre de part</label>
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+    </form>
+<div class="album py-5 bg-body-tertiary">
+<div class="container">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <?php
+        for($i=0; $i < 9; $i++){
+            ?>
+            <div class="col">
+                <div class="card shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body">
+                        <p class="card-text">
+                        </p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="index_.php?id_categorie=&page=produits_categorie.php" type="button" class="btn btn-sm btn-outline-secondary" >View</a>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
+</div>
