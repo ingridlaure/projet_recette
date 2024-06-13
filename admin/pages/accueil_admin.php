bd49d3e2c1851fbc0ab8269b67b1bae426d8ebb3
ingrid <ingrid.ngounekenfack@condorce.be>
2024-06-13 08:31:51 +0200

    version finale

--- admin/pages/accueil_admin.php
+++ admin/pages/accueil_admin.php
@@ -2,5 +2,8 @@
 require 'src/php/utils/verifier_connexion.php';
 ?>
 
-<h2>Bienvenu dans la page administrateur du site gestion des recettes</h2>
+<h2 class="text-center">Bienvenu dans la page administrateur du site gestion des recettes</h2>
+<div class="text-center">
+<img src="./public/images/fond.jpg" alt="image de fond">
+</div>
 
